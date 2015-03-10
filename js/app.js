02157ae6e9f4cefce3b4721e340b62e2cab17235
jlundgren77 <jlundgren77@gmail.com>
2015-03-10 08:48:42 -0400

    fixed double image issue

--- js/app.js
+++ js/app.js
@@ -1,46 +1,61 @@
 $(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+        
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+        
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 
-	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	});
+});
 
-	$('.ryu').mousedown(function(){
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		
-		$('.hadouken').finish().show().animate(
-			{ 'left': '1020px'},
-			500,
-			function() {
-				$(this).hide();
-				$(this).css('left', '520px');
-				}	
-			);
-	})
-	.mouseup(function(){
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-
-	});
-
-	$(document).keydown(function (event){
+$(document).keydown(function (event){
 		if (event.which == 88) {
-			$('.ryu-still').hide();
-			$('.ryu-cool').show();
+			    
+			    if ($('.ryu-ready').is(':visible'))
+			    	$('.ryu-ready').hide();
+			    	$('.ryu-cool').show();
+			
+				$('.ryu-still').hide();
+				
+				$('.ryu-cool').show();
+			
+			
 		}
 	})
 	.keyup(function (event) {
-		$('.ryu-cool').hide();
-		$('.ryu-still').show();
+		if (event.which == 88) {
+			$('.ryu-cool').hide();
+			if ($('.ryu').is(':hover')) 
+				$('.ryu-ready').show();
+			
+			
+		    else 
+		    	$('.ryu-still').show();
+		}
 	});
+	
 
-});
+
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume=0.5;
